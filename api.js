c45ee27e7bb72fc7293e9ae0b959b5b0103cf953
DeepenLau <lpd09009@gmail.com>
2017-07-18 10:12:19 +0800

    fix: 图源网站改了结构

--- api.js
+++ api.js
@@ -7,8 +7,8 @@ async function getImageList(keyword) {
   let list = []
   await axios.get('/', { params: { keyword: keyword } }).then((res) => {
     const $ = cheerio.load(res.data)
-    const data = $('#pic-detail img')
-    $('#pic-detail img').each((i, item) => {
+    const data = $('.search-result img')
+    $('.search-result img').each((i, item) => {
       const isGifIcon = $(item).hasClass('gif')
       if (!isGifIcon) {
         const imageUrl = $(item).data('original')
